61a5c28eea3c6ba1defe7a51db83903b0d27ddb3
JajaS5 <sitova.jc@seznam.cz>
2025-12-24 13:57:10 +0100

    Create Table_2.sql

---
+++ Table_2.sql
@@ -0,0 +1,19 @@
+CREATE TABLE t_jana_sitova_sql_primary_secondary_final AS 
+SELECT
+    cou.country,
+    eco.year,
+    eco.population,
+    eco.gini,
+    eco.gdp
+FROM countries AS cou
+INNER JOIN economies AS eco
+    ON eco.country = cou.country
+WHERE cou.continent = 'Europe'
+  AND eco.year BETWEEN 2006 AND 2018
+ORDER BY
+    cou.country,
+    eco.year;
+
+--CHECK--
+SELECT*
+FROM t_jana_sitova_sql_primary_secondary_final;
