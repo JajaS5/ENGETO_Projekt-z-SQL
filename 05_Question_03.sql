9c65a7c452e5009389021a400a6d9d95f88eff76
JajaS5 <sitova.jc@seznam.cz>
2025-12-28 11:24:49 +0100

    Update 05_Question_03.sql

--- 05_Question_03.sql
+++ 05_Question_03.sql
@@ -21,23 +21,5 @@ ORDER BY price_diff_percent DESC;
 ---další--
 
 
-WITH price_changes AS (
-         SELECT
-                goods,
-                yr_pay,
-               avg_price,
-               LAG(avg_price) OVER (
-            PARTITION BY goods ORDER BY yr_pay) AS prev_price
-    FROM t_jana_sitova_project_sql_primary_final
-)
-SELECT
-            goods,
-            yr_pay AS year,
-           prev_price,
-          avg_price,
-          ROUND(((avg_price - prev_price) / prev_price) * 100, 2) AS yoy_growth_percent
-FROM price_changes
-WHERE prev_price IS NOT NULL
-ORDER BY yoy_growth_percent ASC, goods, year;
 
 
