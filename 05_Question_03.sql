ba90bf9ad85d281be5877a9e1e5f2ca35e34d3d6
JajaS5 <sitova.jc@seznam.cz>
2025-12-28 12:32:57 +0100

    Update 05_Question_03.sql

--- 05_Question_03.sql
+++ 05_Question_03.sql
@@ -19,6 +19,31 @@ GROUP BY goods
 ORDER BY price_diff_percent DESC;
 
 ---Celkový průměr meziročních změn (pokles/růst) ceny potravin v %--
+WITH yearly_prices AS (
+           SELECT
+                       goods,
+                       yr_pay,
+                       AVG(avg_price) AS avg_price_year
+           FROM t_jana_sitova_project_sql_primary_final
+          GROUP BY goods, yr_pay),
+price_changes AS (
+          SELECT
+                      goods,
+                      yr_pay,
+                      avg_price_year,
+                      LAG(avg_price_year) OVER (
+                      PARTITION BY goods
+                      ORDER BY yr_pay) AS prev_price
+           FROM yearly_prices)
+  SELECT
+               goods,
+                ROUND(AVG((avg_price_year - prev_price)/ NULLIF(prev_price, 0) * 100), 2) AS avg_yoy_growth_percent
+FROM price_changes
+WHERE prev_price IS NOT NULL
+GROUP BY goods
+ORDER BY avg_yoy_growth_percent ASC;
+
+
 
 
 
