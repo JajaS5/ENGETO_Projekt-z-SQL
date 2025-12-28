54b34455076752aee4840cf51c2cf07ff4eea47e
JajaS5 <sitova.jc@seznam.cz>
2025-12-28 15:08:35 +0100

    Update 03_Question_01.sql

--- 03_Question_01.sql
+++ 03_Question_01.sql
@@ -5,7 +5,8 @@ SELECT
               br_name,
               ROUND(MAX(CASE WHEN yr_pay = 2006 THEN avg_payroll END), 2) AS payroll_2006,
               ROUND(MAX(CASE WHEN yr_pay = 2018 THEN avg_payroll END), 2) AS payroll_2018,
-              ROUND((MAX(CASE WHEN yr_pay = 2018 THEN avg_payroll END)- MAX(CASE WHEN yr_pay = 2006 THEN avg_payroll END))/ NULLIF(MAX(CASE WHEN yr_pay = 2006 THEN avg_payroll END), 0) *100,2) AS payroll_change_percent
+              ROUND((MAX(CASE WHEN yr_pay = 2018 THEN avg_payroll END)- MAX(CASE WHEN yr_pay = 2006 THEN avg_payroll END))/ 
+              NULLIF(MAX(CASE WHEN yr_pay = 2006 THEN avg_payroll END), 0) *100,2) AS payroll_change_percent
 FROM t_jana_sitova_project_sql_primary_final
 WHERE yr_pay IN (2006, 2018)
 GROUP BY br_name
