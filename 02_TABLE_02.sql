677ec0aaa7d5938688d066fca0187a54acaa0c50
JajaS5 <sitova.jc@seznam.cz>
2025-12-30 13:51:58 +0100

    Update 02_TABLE_02.sql

--- 02_TABLE_02.sql
+++ 02_TABLE_02.sql
@@ -2,22 +2,20 @@
 
 DROP TABLE IF EXISTS t_jana_sitova_sql_secondary_final;
 
-
-CREATE TABLE t_jana_sitova_sql_primary_secondary_final AS 
-SELECT
-    cou.country,
-    eco.year,
-    eco.population,
-    eco.gini,
-    eco.gdp
-FROM countries AS cou
-INNER JOIN economies AS eco
-    ON eco.country = cou.country
-WHERE cou.continent = 'Europe'
-  AND eco.year BETWEEN 2006 AND 2018
-ORDER BY
-    cou.country,
-    eco.year;
+CREATE TABLE t_jana_sitova_sql_primary_secondary_final AS
+               SELECT
+                          cou.country,
+                          eco.year,
+                          AVG(eco.population) AS population,
+                         AVG(eco.gini) AS gini,
+                         AVG(eco.gdp) AS gdp
+                FROM countries AS cou
+JOIN economies AS eco
+               ON eco.country = cou.country
+               WHERE cou.continent = 'Europe'
+               AND eco.year BETWEEN 2006 AND 2018
+              GROUP BY cou.country, eco.year
+              ORDER BY cou.country, eco.year;
 
 --CHECK--
 SELECT*
