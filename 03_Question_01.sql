0e4ccc31610ccefa5f569edfadeef9de0cb0bf96
JajaS5 <sitova.jc@seznam.cz>
2026-01-06 20:07:38 +0100

    Update 03_Question_01.sql

--- 03_Question_01.sql
+++ 03_Question_01.sql
@@ -2,52 +2,52 @@
 
 --Porovnání mezd za první 2006) a poslední (2018) srovnatelné období--
 SELECT
-              br_name,
-              ROUND(MAX(CASE WHEN yr_pay = 2006 THEN avg_payroll END), 2) AS payroll_2006,
-              ROUND(MAX(CASE WHEN yr_pay = 2018 THEN avg_payroll END), 2) AS payroll_2018,
-              ROUND((MAX(CASE WHEN yr_pay = 2018 THEN avg_payroll END)- MAX(CASE WHEN yr_pay = 2006 THEN avg_payroll END))/ 
-              NULLIF(MAX(CASE WHEN yr_pay = 2006 THEN avg_payroll END), 0) *100,2) AS payroll_change_percent
+              branch,
+              ROUND(MAX(CASE WHEN year = 2006 THEN avg_payroll END), 2) AS payroll_2006,
+              ROUND(MAX(CASE WHEN year = 2018 THEN avg_payroll END), 2) AS payroll_2018,
+              ROUND((MAX(CASE WHEN year = 2018 THEN avg_payroll END)- MAX(CASE WHEN yr_pay = 2006 THEN avg_payroll END))/ 
+              NULLIF(MAX(CASE WHEN year = 2006 THEN avg_payroll END), 0) *100,2) AS payroll_change_percent
 FROM t_jana_sitova_project_sql_primary_final
-WHERE yr_pay IN (2006, 2018)
-GROUP BY br_name
+WHERE year IN (2006, 2018)
+GROUP BY branch
 ORDER BY payroll_change_percent;
 
 --Zjištění poklesu mezd, klesají mzdy????---
 WITH yearly_payroll AS (
         SELECT
-                    br_name,
-                    yr_pay,
+                    branch,
+                    year,
                     AVG(avg_payroll) AS avg_payroll_year
          FROM t_jana_sitova_project_sql_primary_final
-        WHERE yr_pay BETWEEN 2006 AND 2018
-       GROUP BY br_name, yr_pay),
+        WHERE year BETWEEN 2006 AND 2018
+       GROUP BY branch, year),
 payroll_changes AS (
          SELECT
-                     br_name,
-                     yr_pay,
+                     brancg,
+                     year,
                      avg_payroll_year,
-                     LAG(avg_payroll_year) OVER (PARTITION BY br_name ORDER BY yr_pay) AS prev_payroll
+                     LAG(avg_payroll_year) OVER (PARTITION BY branch ORDER BY year) AS prev_payroll
        FROM yearly_payroll
 )
        SELECT
-                   br_name,
+                   branch,
                    CASE
                             WHEN MIN(avg_payroll_year - prev_payroll) < 0 THEN 'ANO, někdy klesaly'
                             ELSE 'NE, pouze rostly' END AS payroll_trend
      FROM payroll_changes
     WHERE prev_payroll IS NOT NULL
-    GROUP BY br_name
+    GROUP BY branch
     ORDER BY payroll_trend;
 
 --Pokles mezd, meziroční pokles--
 WITH yearly_payroll AS (
           SELECT
-                   br_name AS industry_branch,
-                   yr_pay AS year,
+                   branch AS industry_branch,
+                   year AS year,
                    AVG(avg_payroll) AS avg_payroll_year
                   FROM t_jana_sitova_project_sql_primary_final
-           WHERE yr_pay BETWEEN 2006 AND 2018
-           GROUP BY br_name, yr_pay),
+           WHERE year BETWEEN 2006 AND 2018
+           GROUP BY branch, year),
 payroll_changes AS (
           SELECT
                    industry_branch,
