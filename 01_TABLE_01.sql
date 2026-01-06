2f3d5f22d2a513797396b3fa471eaca60b8a64fc
JajaS5 <sitova.jc@seznam.cz>
2026-01-06 18:23:39 +0100

    Clean up SQL file by removing unused queries
    Removed several SELECT statements and comments from the SQL file.

--- 01_TABLE_01.sql
+++ 01_TABLE_01.sql
@@ -27,44 +27,5 @@ GROUP BY year, ID_branch, branch, ID_goods, goods
 ORDER BY year, ID_branch, ID_goods
 ;
 
---ZKOUSKA--
-SELECT *
-FROM t_jana_sitova_sql_primary_final;
 
---POMOCNE SELECTY--
-
---Zjistení min/max--
-
-SELECT
-      min (payroll_year), 
-      max (payroll_year)
- FROM czechia_payroll;
---output 2000, 2021--
-
-SELECT
-      min (date_from),
-      max (date_to)
-FROM czechia_price;
---output 2006, 2018--
-
-SELECT
-     min (year),
-     max (year)
-FROM economies; 
---output 1960, 2020--
-
---Pocet 0 ve sloupecku--
-
-SELECT COUNT(*) AS pocet_radku
-FROM czechia_payroll
-WHERE value_type_code = 5958
-AND industry_branch_code IS NULL;
---output 172-- 
- 
---overení UNIT CODE--
-SELECT DISTINCT unit_code
-FROM czechia_payroll
-WHERE value_type_code = 5958;
---output 200--
---prumerna mzda--unit code=200--
 
