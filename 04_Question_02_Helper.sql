c8d113d1363b2e81f06f6741ae865ceb25a45b9c
JajaS5 <sitova.jc@seznam.cz>
2026-01-06 22:24:42 +0100

    Add SQL queries for product selection and validation

--- 04_Question_02_Helper.sql
+++ 04_Question_02_Helper.sql
@@ -1,1 +1,13 @@
+--pomocny SELECT--code mleko_chleb--
+SELECT 
+	cpc.name,
+	cpc.code
+FROM czechia_price_category cpc 
+WHERE cpc.name LIKE 'Chléb%' OR cpc.name LIKE 'Mléko%'
+;
+--output--chleb 111301--mleko 114201--
 
+---overeni nul--
+SELECT COUNT(*) AS pocet_radku
+FROM t_jana_sitova_project_sql_primary_final
+WHERE id_branch IS NULL;
