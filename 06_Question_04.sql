946abb208f11dc7ab1fff4ffe5152f60998046eb
JajaS5 <sitova.jc@seznam.cz>
2026-01-08 18:54:07 +0100

    Update 06_Question_04.sql

--- 06_Question_04.sql
+++ 06_Question_04.sql
@@ -3,31 +3,28 @@
 --průměrná mzda v daném roce--průměrná cena v daném roce--
 CREATE OR REPLACE VIEW t_jasi_yearly_values AS
 SELECT
-            year,
-            AVG(avg_payroll) AS avg_payroll_year,
-            AVG(avg_price) AS avg_price_year
+      year,
+      AVG(avg_payroll) AS avg_payroll_year,
+      AVG(avg_price) AS avg_price_year
 FROM t_jana_sitova_project_SQL_primary_final
 GROUP BY year
 ORDER BY year;       
 
 --mezroční růst price/payroll--rozdíl--
 WITH yearly_growth AS (
-           SELECT
-                      year,
-                      avg_payroll_year,
-                      avg_price_year,
-                      LAG(avg_payroll_year) OVER (ORDER BY year) AS prev_payroll,
-                      LAG(avg_price_year) OVER (ORDER BY year) AS prev_price
-           FROM t_jasi_yearly_values)
-          SELECT
-                     year,
-                     ROUND(((avg_price_year - prev_price) / prev_price) * 100, 2) AS growth_price_percent,
-                     ROUND(((avg_payroll_year - prev_payroll) / prev_payroll) * 100, 2) AS growth_payroll_percent,
-    ROUND(
-        ((avg_price_year - prev_price) / prev_price) * 100
-        - ((avg_payroll_year - prev_payroll) / prev_payroll) * 100
-    , 2) AS difference_percent
-           FROM yearly_growth
-          WHERE prev_payroll IS NOT NULL
-           ORDER BY year;
+SELECT
+     year,
+     avg_payroll_year,
+     avg_price_year,
+     LAG(avg_payroll_year) OVER (ORDER BY year) AS prev_payroll,
+     LAG(avg_price_year) OVER (ORDER BY year) AS prev_price
+FROM t_jasi_yearly_values)
+SELECT
+      year,
+      ROUND(((avg_price_year - prev_price) / prev_price) * 100, 2) AS growth_price_percent,
+      ROUND(((avg_payroll_year - prev_payroll) / prev_payroll) * 100, 2) AS growth_payroll_percent,
+      ROUND(((avg_price_year - prev_price) / prev_price) * 100- ((avg_payroll_year - prev_payroll) / prev_payroll) * 100, 2) AS difference_percent
+FROM yearly_growth
+WHERE prev_payroll IS NOT NULL
+ORDER BY year;
 
