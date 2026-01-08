755356bef31fe76b9c7804914a742dca83c48221
JajaS5 <sitova.jc@seznam.cz>
2026-01-08 18:19:09 +0100

    Fix SQL query year references for price comparison

--- 05_Question_03.sql
+++ 05_Question_03.sql
@@ -3,20 +3,15 @@
 --Porovnaní ceny za první (2006) a poslední (2018) srovnatelene období--
 SELECT
             goods,
-            MAX(CASE WHEN yr_pay = 2006 THEN avg_price END) AS price_2006,
-            MAX(CASE WHEN yr_pay = 2018 THEN avg_price END) AS price_2018,
-            ROUND(
-            (
-            MAX(CASE WHEN yr_pay = 2018 THEN avg_price END)
-            - MAX(CASE WHEN yr_pay = 2006 THEN avg_price END)
-        )
-        / NULLIF(MAX(CASE WHEN yr_pay = 2006 THEN avg_price END), 0) * 100,
-        2
-    ) AS price_diff_percent
+            MAX(CASE WHEN year = 2006 THEN avg_price END) AS price_2006,
+            MAX(CASE WHEN year = 2018 THEN avg_price END) AS price_2018,
+            ROUND(( MAX(CASE WHEN year= 2018 THEN avg_price END)- MAX(CASE WHEN year = 2006 THEN avg_price END))
+            / NULLIF(MAX(CASE WHEN year = 2006 THEN avg_price END), 0) * 100,2) AS price_diff_percent
 FROM t_jana_sitova_project_SQL_primary_final
-WHERE yr_pay IN (2006, 2018)
+WHERE year IN (2006, 2018)
 GROUP BY goods
 ORDER BY price_diff_percent DESC;
+
 
 ---Celkový průměr meziročních změn (pokles/růst) ceny potravin v %--
 WITH yearly_prices AS (
