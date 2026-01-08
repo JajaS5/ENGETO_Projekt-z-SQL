140686db3f0432c7bc1906b77c6c63852b1d813c
JajaS5 <sitova.jc@seznam.cz>
2026-01-08 18:58:52 +0100

    Update 05_Question_03.sql

--- 05_Question_03.sql
+++ 05_Question_03.sql
@@ -14,28 +14,28 @@ ORDER BY price_diff_percent DESC;
 
 ---Celkový průměr meziročních změn (pokles/růst) ceny potravin v %--
 WITH yearly_prices AS (
-           SELECT
+SELECT
                 goods,
                 year,
                 AVG(avg_price) AS avg_price_year
            FROM t_jana_sitova_project_SQL_primary_final
            GROUP BY goods, year),
 price_changes AS (
-            SELECT
-                 goods,
-                 year,
-                 avg_price_year,
-                 LAG(avg_price_year) OVER (
-                 PARTITION BY goods
-                  ORDER BY year) AS prev_price
-           FROM yearly_prices)
-           SELECT
-                 goods,
-                 ROUND(AVG((avg_price_year - prev_price)/ NULLIF(prev_price, 0) * 100), 2) AS avg_yoy_growth_percent
-           FROM price_changes
-           WHERE prev_price IS NOT NULL
-           GROUP BY goods
-           ORDER BY avg_yoy_growth_percent ASC;
+SELECT
+      goods,
+      year,
+      avg_price_year,
+      LAG(avg_price_year) OVER (
+      PARTITION BY goods
+ORDER BY year) AS prev_price
+FROM yearly_prices)
+SELECT
+     goods,
+     ROUND(AVG((avg_price_year - prev_price)/ NULLIF(prev_price, 0) * 100), 2) AS avg_yoy_growth_percent
+FROM price_changes
+WHERE prev_price IS NOT NULL
+GROUP BY goods
+ORDER BY avg_yoy_growth_percent ASC;
 
 --minimum meziroční nárůst--
 WITH yearly_prices AS (
