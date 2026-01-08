9f7e504ff166cee064c1f12d96abb746d3b705ef
JajaS5 <sitova.jc@seznam.cz>
2026-01-08 19:05:49 +0100

    Update 07_Question_05.sql

--- 07_Question_05.sql
+++ 07_Question_05.sql
@@ -3,19 +3,19 @@
 --meziroční % změny mezd a cen potravin--
 CREATE OR REPLACE VIEW v_price_payroll_changes AS
 WITH changes AS (
-           SELECT
-                         year,
-                         avg_payroll_year,
-                         avg_price_year,
-                         LAG(avg_payroll_year) OVER (ORDER BY year) AS prev_payroll,
-                         LAG(avg_price_year) OVER (ORDER BY year) AS prev_price
-             FROM t_jasi_yearly_values)
-            SELECT
-                       year,
-                       ROUND(((avg_payroll_year - prev_payroll) / prev_payroll) * 100, 2) AS payroll_growth_pct,
-                       ROUND(((avg_price_year - prev_price) / prev_price) * 100, 2) AS food_price_growth_pct
-                      FROM changes
-                      WHERE prev_payroll IS NOT NULL;
+SELECT
+      year,
+      avg_payroll_year,
+      avg_price_year,
+      LAG(avg_payroll_year) OVER (ORDER BY year) AS prev_payroll,
+      LAG(avg_price_year) OVER (ORDER BY year) AS prev_price
+FROM t_jasi_yearly_values)
+SELECT
+     year,
+     ROUND(((avg_payroll_year - prev_payroll) / prev_payroll) * 100, 2) AS payroll_growth_pct,
+     ROUND(((avg_price_year - prev_price) / prev_price) * 100, 2) AS food_price_growth_pct
+FROM changes
+WHERE prev_payroll IS NOT NULL;
 
 
 --Meziroční % změna GDP za ČR--
